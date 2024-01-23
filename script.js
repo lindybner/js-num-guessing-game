9871854e6d42d150cdc8225f30df77e827da9526
Jim Lin-Dybner <lin.jycl@gmail.com>
2024-01-23 13:39:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,25 @@
     Write a JavaScript program where the program takes a random integer between 1 and 10, and the user is then prompted to input a guess number. The program displays a message "Good Work" if the input matches the guess number otherwise "Not matched".
 */
 
-// var
-const msgPrompt = `Guess a number between 0 and 10 inclusive.`;
-const msgSuccess = `Good Work`;
+const msgPrompt = `Guess a number between 1 and 10 inclusive.`;
+const msgSuccess = `Good Work!`;
 const msgFail = `Not matched`;
 
 let num = Math.ceil(Math.random() * 10);
 console.log(num);
 
-let guess = parseInt(prompt(msgPrompt));
+let guess;
 
-if (guess === num) {
-  alert(msgSuccess);
-} else {
-  alert(msgFail);
-}
+do {
+  guess = parseInt(prompt(msgPrompt));
+
+  if (isNaN(guess)) {
+    alert(`Please enter a valid number.`);
+  } else {
+    if (guess === num) {
+      alert(msgSuccess);
+    } else {
+      alert(msgFail);
+    }
+  }
+} while (guess !== num);
